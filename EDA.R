006c0abf3ceac7c21805a1b560b6bb580fab4181
TabithaMasha <125862586+TabithaMasha@users.noreply.github.com>
2025-06-10 23:53:20 +0200

    Add files via upload

---
+++ EDA.R
@@ -0,0 +1,16 @@
+#Loading libraries
+library(dplyr)
+library(plyr)
+library(tidyr)
+library(ggplot2)
+#Rename variables
+rename_var <- c("age_group" = "age_group2")
+GPSJS_dataset <- GPSJS_dataset %>%
+  rename(!!!rename_var)
+#Chi-square tests and residuals
+crime_table<-table(GPSJS_dataCleaned$maritalstatus,GPSJS_dataCleaned$allCrimes)
+crime_table
+chi_test <- chisq.test(crime_table)
+chi_test
+residuals <- chisq.test(crime_table)$stdres
+print(residuals)
