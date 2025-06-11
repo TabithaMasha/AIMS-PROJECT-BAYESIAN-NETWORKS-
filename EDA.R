a2dc242414a5514741bb3c92221be7fb017caf05
TabithaMasha <125862586+TabithaMasha@users.noreply.github.com>
2025-06-11 16:08:44 +0200

    EDA.R

--- EDA.R
+++ EDA.R
@@ -4,7 +4,6 @@ library(plyr)
 library(tidyr)
 library(ggplot2)
 # Plot for provincial counts
-# Plot
 ggplot(GPSJS_dataset, aes(x = prov, fill = prov)) +
   geom_bar() +
   labs(
@@ -16,9 +15,11 @@ ggplot(GPSJS_dataset, aes(x = prov, fill = prov)) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 #Personal Property rates
-PPT_rates <- GPSJS_dataset %>%
-  group_by(prov) %>%
-  summarise(
+PPT_rates <- summarise(
+  group_by(
+     GPSJS_dataset,
+    prov
+  ),
     Yes = sum(Personal_Property== "1", na.rm = TRUE),
     TotalResponses = n(),
     Rate = Yes / TotalResponses
@@ -26,69 +27,86 @@ PPT_rates <- GPSJS_dataset %>%
 # View the table
 print(PPT_rates)
 #Robbery rates in provinces
-Rob_rates <- GPSJS_dataset %>%
-  group_by(prov) %>%
-  summarise(
-    Yes = sum(Robbery== "1", na.rm = TRUE),
+Rob_rates <- summarise(
+  group_by(
+     GPSJS_dataset,
+    prov
+  ),
+  Yes = sum(Robbery== "1", na.rm = TRUE),
     TotalResponses = n(),
     Rate = Yes / TotalResponses
   )
+    
 #Consumer Fraud rates in provinces
-CF_rates <- GPSJS_dataset %>%
-  group_by(prov) %>%
-  summarise(
+CF_rates <-summarise(
+  group_by(
+     GPSJS_dataset,
+    prov
+  ),
     Yes = sum(Consumer_Fraud== "1", na.rm = TRUE),
     TotalResponses = n(),
     Rate = Yes / TotalResponses
   )
 #Hijacking/Other rates in provinces
-Hij/Other_rates <- GPSJS_dataset %>%
-  group_by(prov) %>%
-  summarise(
+Hij/Other_rates <-summarise(
+  group_by(
+     GPSJS_dataset,
+    prov
+  ),
     Yes = sum(Hijacking_Other== "1", na.rm = TRUE),
     TotalResponses = n(),
     Rate = Yes / TotalResponses
   )
 #Rates by Education level
 #Personal Property rates by education level
-PPT_rates <- GPSJS_dataset %>%
-  group_by(education_level) %>%
-  summarise(
+PPT_rates <- summarise(
+  group_by(
+     GPSJS_dataset,
+    education_level
+  ),
     Yes = sum(Personal_Property== "1", na.rm = TRUE),
     TotalResponses = n(),
     Rate = Yes / TotalResponses
   )
-# View the table
+
 print(PPT_rates)
 #Robbery rates by education level
-Rob_rates <- GPSJS_dataset %>%
-  group_by(education_level) %>%
-  summarise(
+Rob_rates <- summarise(
+  group_by(
+     GPSJS_dataset,
+    education_level
+  ),
     Yes = sum(Robbery== "1", na.rm = TRUE),
     TotalResponses = n(),
     Rate = Yes / TotalResponses
   )
 #Consumer Fraud rates by education level
-CF_rates <- GPSJS_dataset %>%
-  group_by(education_level) %>%
-  summarise(
+CF_rates <- summarise(
+  group_by(
+     GPSJS_dataset,
+    education_level
+  ),
     Yes = sum(Consumer_Fraud== "1", na.rm = TRUE),
     TotalResponses = n(),
     Rate = Yes / TotalResponses
   )
 #Hijacking/Other rates by education level
-Hij/Other_rates <- GPSJS_dataset %>%
-  group_by(education_level) %>%
-  summarise(
+Hij/Other_rates <-summarise(
+  group_by(
+     GPSJS_dataset,
+    education_level
+  ),
     Yes = sum(Hijacking_Other== "1", na.rm = TRUE),
     TotalResponses = n(),
     Rate = Yes / TotalResponses
   )
 #Rates by marital status
 #Personal Property rates by marital status
-PPT_rates <- GPSJS_dataset %>%
-  group_by(maritalstatus) %>%
-  summarise(
+PPT_rates <- summarise(
+  group_by(
+     GPSJS_dataset,
+    maritalstatus
+  ),
     Yes = sum(Personal_Property== "1", na.rm = TRUE),
     TotalResponses = n(),
     Rate = Yes / TotalResponses
@@ -96,25 +114,31 @@ PPT_rates <- GPSJS_dataset %>%
 # View the table
 print(PPT_rates)
 #Robbery rates by marital status
-Rob_rates <- GPSJS_dataset %>%
-  group_by(maritalstatus) %>%
-  summarise(
+Rob_rates <- summarise(
+  group_by(
+     GPSJS_dataset,
+    maritalstatus
+  ),
     Yes = sum(Robbery== "1", na.rm = TRUE),
     TotalResponses = n(),
     Rate = Yes / TotalResponses
   )
 #Consumer Fraud rates by marital status
-CF_rates <- GPSJS_dataset %>%
-  group_by(maritalstatus) %>%
-  summarise(
+CF_rates <- summarise(
+  group_by(
+     GPSJS_dataset,
+    maritalstatus
+  ),
     Yes = sum(Consumer_Fraud== "1", na.rm = TRUE),
     TotalResponses = n(),
     Rate = Yes / TotalResponses
   )
 #Hijacking/Other rates by marital status
-Hij/Other_rates <- GPSJS_dataset %>%
-  group_by(maritalstatus) %>%
-  summarise(
+Hij/Other_rates <-summarise(
+  group_by(
+     GPSJS_dataset,
+    maritalstatus
+  ),
     Yes = sum(Hijacking_Other== "1", na.rm = TRUE),
     TotalResponses = n(),
     Rate = Yes / TotalResponses
@@ -122,31 +146,43 @@ Hij/Other_rates <- GPSJS_dataset %>%
 #ercentage of crime occurrence distribution in province
 crime_vars <- c("Robbery", "Personal_Property", "Consumer_Fraud","Hijacking_Other")
 # Calculating the  total number of crimes
-total_crimes <- GPSJS_dataset %>%
-  pivot_longer(cols = all_of(crime_vars), names_to = "Crimetype", values_to = "Response") %>%
-  filter(Response == "1") %>%
-  nrow()
-
-# Calculating the number of crimes and rate  in a province by crime type
-crime_rates <- GPSJS_dataset %>%
-  pivot_longer(cols = all_of(crime_vars), names_to = "Crimetype", values_to = "Response") %>%
-  filter(Response == "1") %>%
-  group_by(prov, Crimetype) %>%
-  summarise(
-    NumCrimes = n(),
-    TotalCrimes = total_crimes,
-    Rate = NumCrimes / TotalCrimes
+total_crimes <- nrow(
+  filter(
+    pivot_longer(
+      GPSJS_dataset,
+      cols = all_of(crime_vars),
+      names_to = "Crimetype",
+      values_to = "Response"
+    ),
+    Response == "1"
+    )
   )
 
-# View the table
+# Calculating the number of crimes and rate  in a province by crime type
+crime_rates <-summarise(
+  groupby(
+    filter(
+      pivot_longer(
+        GPSJS_dataset,
+      cols = all_of(crime_vars),
+      names_to = "Crimetype",
+      values_to = "Response"
+    ),
+    Response == "1"
+    ),
+    prov, Crimetype
+    ),
+  NumCrimes = n(),
+  Total_Crimes= total_crimes,
+  Rate =  NumCrimes/Total_Crimes
+
 print(crime_rates)
 
 
 
 #Rename variables
 rename_var <- c("age_group" = "age_group2")
-GPSJS_dataset <- GPSJS_dataset %>%
-  rename(!!!rename_var)
+GPSJS_dataset <- rename(GPSJS_dataset, !!!rename_var)
 #Chi-square tests and residuals
 crime_table<-table(GPSJS_dataCleaned$maritalstatus,GPSJS_dataCleaned$allCrimes)
 crime_table
